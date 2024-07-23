386f3a022ff887750f094030c8fe8d7cab41db0c
김건우 <96743351+kimgunwooo@users.noreply.github.com>
2024-07-23 11:01:44 +0900

    fix : 파일 저장 필드 변경 & 조회수 로직 추가

--- src/main/java/com/fizz/fizz_server/domain/post/dto/response/PostInfo.java
+++ src/main/java/com/fizz/fizz_server/domain/post/dto/response/PostInfo.java
@@ -2,6 +2,7 @@ package com.fizz.fizz_server.domain.post.dto.response;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fizz.fizz_server.domain.post.domain.Post;
+import com.fizz.fizz_server.domain.post.domain.vo.FileType;
 import lombok.Builder;
 
 import java.time.LocalDateTime;
@@ -12,7 +13,9 @@ public record PostInfo(
         Long id,
         String title,
         String content,
+        FileType fileType,
         List<String> fileUrls,
+        int viewCount,
         int commentCount,
         int likeCount,
         @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
@@ -31,7 +34,9 @@ public record PostInfo(
                 .id(post.getId())
                 .title(post.getTitle())
                 .content(post.getContent())
+                .fileType(post.getFileType())
                 .fileUrls(post.getFileUrls())
+                .viewCount(post.getViews().size())
                 .commentCount(post.getComments().size())
                 .likeCount(post.getPostLikes().size())
                 .createdAt(post.getCreatedAt())
